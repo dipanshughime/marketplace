5c2aa2104f99eee6db348730503a8c579f5f60b2
Dipanshu ghime <dipanshughime@gmail.com>
2023-06-07 23:39:24 +0530

    login created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,49 @@
-const user=require("../models/userModels")
+const User=require("../models/userModels")
 const catchAsyncError= require("../middleware/catchAsyncError");
-
+const ErrorHander = require("../utils/errorhandler");
 
 // Register a User
-exports.registerUser=catchAsyncError(async(req,resp,next)=>{
+exports.registerUser=catchAsyncError(async(req,res,next)=>{
  const {name ,email,password}=req.body;
- const User = await user.create({
+ const user = await User.create({
   name ,email,password,
   avatar:{
     public_id :"this is sample id ",
     url : "profilepicurl"
   }
  });
- const token=User.getJWTToken();
-
-
-
- resp.status(201).json({
-    success:true,
-    token,
- })
+ const token=user.getJWTToken();
+ res.status(201).json({
+  success:true,
+  token,
+})
 });
+
+
+ // login user
+ exports.loginUser=catchAsyncError(async(req,res,next)=>{
+  const {email,password}=req.body;
+  
+  // check if user has given password and email
+  
+  if(!password || !email){
+    return next( new ErrorHander ( "Please Enter email and password ",400));
+  }
+ const user = await User.findOne({email}).select("+password");
+
+ if(!User){
+  return next(new ErrorHander("Invalid email or password",401));
+ }
+ const ispasswordMatched = await user.comparePassword(password);
+ if(!ispasswordMatched){
+  return next(new ErrorHander("Invalid email or password",401)); 
+ } 
+ 
+ token=user.getJWTToken();
+ res.status(201).json({
+   success:true,
+   token,
+  })
+  
+});
+
--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -63,4 +63,10 @@ userSchema.methods.getJWTToken = function () {
   });
 };
 
+
+// compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
     module.exports=mongoose.model("User",userSchema);
